69335d13fb6c88ba250f68b35e7b398bfaad9c12
springcoil <peadarcoyle@googlemail.com>
2022-12-06 15:16:02 +0100

    Updating voice endpoints

--- audiostack/speech/voice.py
+++ audiostack/speech/voice.py
@@ -4,15 +4,16 @@ from audiostack.helpers.api_item import APIResponseItem
 from audiostack.helpers.api_list import APIResponseList
 
 class Voice():
-    interface = RequestInterface(family="speech")
+    interface = RequestInterface(family="speech/voice")
     
     OBJECT_NAME = "voice"
     resource_path = "/voice"
     list_parameters_path = "/voice/parameter"
 
-    @classmethod
-    def list_parameters(cls):
-        return cls._get_request(path_param=cls.list_parameters_path)
+    @staticmethod
+    def get() -> dict:
+        r = Voice.interface.send_request(rtype=RequestTypes.GET, route="parameter")
+        return r
 
 
    
