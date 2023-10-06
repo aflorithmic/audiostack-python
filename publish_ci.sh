c0ae983a18ae9e2407c6b6ecf7597db74141b9f1
hackerman <sam@aflorithmic.ai>
2023-10-06 14:26:07 +0100

    update script

--- publish_ci.sh
+++ publish_ci.sh
@@ -1,11 +1,7 @@
-function deploy {
-    rm -rf audiostack.egg-info build dist
-    pip3 install twine wheel
-    python3 setup.py sdist bdist_wheel
-    echo '============'
-    echo 'In order to obtain the username/pass; contact Core team devs'
-    echo '%%%%%%%%%%%%'
-    python3 -m twine upload dist/*
-    python3 -m twine upload dist/* -u __token__ -p $PIP_AUDIOSTACK_TOKEN
-
-}
+rm -rf audiostack.egg-info build dist
+pip3 install twine wheel
+python3 setup.py sdist bdist_wheel
+echo '============'
+echo 'In order to obtain the username/pass; contact Core team devs'
+echo '%%%%%%%%%%%%'
+python3 -m twine upload dist/* -u __token__ -p $PIP_AUDIOSTACK_TOKEN
