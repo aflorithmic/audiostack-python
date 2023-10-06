cd78f2465715626c56e8e5d400b3a154e823c6b3
hackerman <sam@aflorithmic.ai>
2023-10-06 10:56:03 +0100

    update api_item

--- audiostack/helpers/api_item.py
+++ audiostack/helpers/api_item.py
@@ -4,16 +4,14 @@ import json
 class APIResponseItem:
     def __init__(self, response):
         self.response = response
-        self.status_code = response["statusCode"]
         
-        if "data" in response:
-            self.data = self.response["data"]
-        if "message" in response:
-            self.message = self.response["message"]
-        if "meta" in response:
-            self.meta = self.response["meta"]
-        if "bytes" in response:
-            self.bytes = self.response["bytes"]
+        # extra specific fields
+        self.status_code = response.get("statusCode", 0)
+        self.data = response.get("data", {})
+        self.message = response.get("message", "")
+        self.meta = response.get("meta", {})
+        self.bytes = response.get("bytes", None)
+        
 
     def print_response(self, indent=0):
         if indent:
