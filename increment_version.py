1a236e64a58f06143c89e6732ca447a9e5bf703b
hackerman <sam@aflorithmic.ai>
2023-10-06 14:08:20 +0100

    fix me

--- increment_version.py
+++ increment_version.py
@@ -2,10 +2,12 @@ import os
 
 sdk_version = os.environ["CIRCLE_TAG"]
 print(sdk_version)
-sdk_version.split("v")[-1]
+sdk_version = sdk_version.split("v")[-1]
 
 with open("audiostack/__init__.py", "r") as f:
     lines = f.readlines()
-    lines[0] = f"""sdk_version =  "{sdk_version}"""
+    lines[0] = f"""sdk_version = "{sdk_version}"""
 
-print(lines)
+
+with open("audiostack/__init__.py", "w") as f:
+    f.writelines(lines)
