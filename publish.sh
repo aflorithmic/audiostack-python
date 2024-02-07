062f182f4ef0100917a7aec135579fbab85e1d6c
Lars Heimes <lars@Larss-MacBook-Pro.local>
2024-02-07 11:45:51 +0100

    update sh scripts with instructions how to retrieve the token

--- publish.sh
+++ publish.sh
@@ -1,3 +1,7 @@
+# You can find the token to publish to pypi here 
+# https://eu-west-1.console.aws.amazon.com/systems-manager/parameters/pypi-m2m-token/description?region=eu-west-1&tab=Table#list_parameter_filters=Name:Contains:py
+# run `PIP_AUDIOSTACK_TOKEN=<<token>>` before executing this script
+
 function deploy {
     rm -rf audiostack.egg-info build dist
     pip3 install twine wheel
