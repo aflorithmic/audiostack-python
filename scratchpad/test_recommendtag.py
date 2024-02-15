8ba7814661442ef2e0e3842d42a744b752f9b2ad
Davide Busacca <davide@aflorithmic.ai>
2024-02-15 14:33:23 +0100

    refactoring post -> create in scratchpad

--- scratchpad/test_recommendtag.py
+++ scratchpad/test_recommendtag.py
@@ -10,15 +10,15 @@ tags = ["happy", "sad", "valuable"]
 number_of_results = 2
 
 print("Recommend Tag endpoint")
-item = audiostack.Content.RecommendTag.post(text=text, category=category, tags=tags, number_of_results=number_of_results)
+item = audiostack.Content.RecommendTag.create(text=text, category=category, tags=tags, number_of_results=number_of_results)
 print(item.response)
 
 print("Recommend Tone endpoint")
-item = audiostack.Content.RecommendTone.post(text=text, number_of_results=number_of_results)
+item = audiostack.Content.RecommendTone.create(text=text, number_of_results=number_of_results)
 print(item.response)
 
 print("Recommend Mood endpoint")
-item = audiostack.Content.RecommendMood.post(text=text, number_of_results=number_of_results)
+item = audiostack.Content.RecommendMood.create(text=text, number_of_results=number_of_results)
 print(item.response)
 
 print("Cost for this session: ", audiostack.credits_used_in_this_session())
