f0cecf9c92624dfc70ce19fa2b944c05dfdeb73d
Guillem <37125339+guillem-211@users.noreply.github.com>
2024-04-22 11:55:07 +0100

    addvideo unit tst

--- audiostack/tests/production/test_video.py
+++ audiostack/tests/production/test_video.py
@@ -1,38 +1,24 @@
-import audiostack
 import os
+import audiostack
+
+from audiostack.delivery.video import Video
+
+audiostack.api_base = os.environ.get("AUDIO_STACK_DEV_URL", "https://v2.api.audio")
+audiostack.api_key = os.environ["AUDIO_STACK_DEV_KEY"]
+
+test_constants = {}
 
 
+def test_video():
+    script = audiostack.Content.Script.create(scriptText="hello sam")
 
-# Create a script
-scriptText = """
-Right now, get four thousand five hundred dollars cash allowance on the purchase of virtually ALL twenty twenty four Ram fifteen hundred trucks.
-"""
-voice_name="isaac"
+    speech = audiostack.Speech.TTS.create(scriptItem=script, voice="sara")
 
-script = audiostack.Content.Script.create(
-  scriptText=scriptText,
-  projectName="Demos",
-)
-
-#speech
-tts = audiostack.Speech.TTS.create(
-  scriptItem=script,
-  voice=voice_name,
-  speed=1.00,
-  )
-
-
-mix = audiostack.Production.Mix.create(speechItem=tts, masteringPreset="radio", soundTemplate="hotwheels")
-
-#delivery
-
-video = audiostack.Delivery.Video.create(
-  productionItem=mix,
-  public=True,
-)
-
-file_name = f'video-{voice_name}'
-video.download(fileName=file_name)
-print(video)
-
-print()
+    mix = audiostack.Production.Mix.create(speechItem=speech)
+    print(mix)
+        
+    video = audiostack.Delivery.Video.create(
+        productionItem=mix,
+        public=True,
+    )
+    print(video)
