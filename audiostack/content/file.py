0afa1a1441d7e9a3859c1df25e22a6bd090c6db6
Peadar Coyle <peadarcoyle@googlemail.com>
2024-04-30 15:31:35 +0100

    Update file.py
    Fixed an error in the message

--- audiostack/content/file.py
+++ audiostack/content/file.py
@@ -45,7 +45,7 @@ class File:
         filePath: str = None,
     ) -> Item:
         if not os.path.isfile(localPath):
-            raise Exception("Supplied file does not eixst")
+            raise Exception("Supplied file does not exist")
 
         if not uploadPath or not localPath:
             raise Exception(
