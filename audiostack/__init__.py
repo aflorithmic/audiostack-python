2e1ef66153bfa803ca1d79330979a406c11b557a
Henry Luckhoo <henry@audiostack.ai>
2024-05-20 09:05:06 +0100

    doc: Added python deprecation warning

--- audiostack/__init__.py
+++ audiostack/__init__.py
@@ -1,15 +1,24 @@
-sdk_version = "1.3.0"
+sdk_version = "1.3.1"
 api_base = "https://v2.api.audio"
 api_key = None
 assume_org_id = None
 app_info = None
 
 
+from warnings import warn
+
 from audiostack import content as Content
 from audiostack import speech as Speech
 from audiostack import production as Production
 from audiostack import delivery as Delivery
 from audiostack.docs.docs import Documentation
+
+warn(
+    "Future releases (`^2.0.0`) of the AudioStack SDK will only support python `^3.8.1`",
+    DeprecationWarning,
+    stacklevel=2,
+)
+
 
 billing_session = 0
 
