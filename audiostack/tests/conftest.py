021e2eb0834de0cd5fea6986289e8072b811dc10
Harry Gardiner <harry@audiostack.ai>
2025-08-05 14:23:13 +0100

    type ignore

--- audiostack/tests/conftest.py
+++ audiostack/tests/conftest.py
@@ -8,7 +8,7 @@ import audiostack
 audiostack.api_base = os.environ.get(
     "AUDIO_STACK_DEV_URL", "https://staging-v2.api.audio"
 )
-audiostack.api_key = os.environ.get("AUDIO_STACK_DEV_KEY", None)
+audiostack.api_key = os.environ.get("AUDIO_STACK_DEV_KEY", None)  # type: ignore
 
 
 @pytest.fixture
